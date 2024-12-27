3d39539d5bed1e9464b96aa2054d0ac91ff8329b
Iransx_bjj <iransantana2624@gmail.com>
2024-12-27 17:04:33 -0300

    aula3

---
+++ aula3/ex/index.js
@@ -0,0 +1,3 @@
+console.log('Meu nome é "Iran". Estou aprendendo JavaScript as 10 da manhã.') 
+//Este é o repositorio do gitHub do curso: https://github.com/luizomf/curso-js
+//Este é o repositorio do gitHub do curso: https://github.com/luizomf/curso-js 
