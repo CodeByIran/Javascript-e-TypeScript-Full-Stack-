9ad130e832f4c2edf6aa7626bebb12e60c6be26c
Iransx_bjj <iransantana2624@gmail.com>
2024-12-27 17:04:40 -0300

    aula4

---
+++ aula4/comentarios.js
@@ -0,0 +1,8 @@
+// Este é um comentario uma anotação
+console.log('Olá mundo!'); // Este é outro comentario
+
+console.log('Linha 1'); 
+console.log('Linha 2'); 
+console.log('Linha 3'); 
+console.log('Linha 4'); 
+ 
